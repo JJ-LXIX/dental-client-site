b9419f91fa9d8b2dea1c1504d5b78eeed32f3b69
Jagrath Shetty <jagrathshettylxix@gmail.com>
2023-07-06 00:57:53 +0530

    added hero section to page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
-import Image from 'next/image'
+import HeroSection from "@/components/Hero Section/HeroSection";
 
 export default function Home() {
   return (
     <main className="min-h-screen">
+      <HeroSection />
     </main>
   );
 }
