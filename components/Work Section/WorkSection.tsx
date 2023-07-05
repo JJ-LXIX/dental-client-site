6dd9ef3ef74e54cebee850bcb7992d153c6eb38e
Jagrath Shetty <jagrathshettylxix@gmail.com>
2023-07-06 01:02:27 +0530

    created workSection.tsx

---
+++ components/Work Section/WorkSection.tsx
@@ -0,0 +1,12 @@
+import WorkCard from "./WorkCard";
+
+type Props = {};
+
+function WorkSection({}: Props) {
+  return (
+    <div className="mt-5 mb-5 lg:mt-0 w-full flex justify-center">
+    </div>
+  );
+}
+
+export default WorkSection;
