fce7b56f4007246358a5ead68c22fb6bfd45e997
Jagrath Shetty <jagrathshettylxix@gmail.com>
2023-07-06 01:01:35 +0530

    created work card

---
+++ components/Work Section/WorkCard.tsx
@@ -0,0 +1,12 @@
+import DentalWork from "@/public/images/pexels-anna-shvets-3845765.jpg";
+import Image from "next/image";
+type Props = {};
+
+function WorkCard({}: Props) {
+  return (
+    <div className="h-[18rem] md:h-[25rem] w-[90%] relative group shadow-xl overflow-hidden rounded-2xl">
+    </div>
+  );
+}
+
+export default WorkCard;
