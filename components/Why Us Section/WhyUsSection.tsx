cb20e5eb8ae642fd3769c33145e63738ebb5815c
Jagrath Shetty <jagrathshettylxix@gmail.com>
2023-07-06 00:58:59 +0530

    created WhyUsSection.tsx

---
+++ components/Why Us Section/WhyUsSection.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import React from "react";
+import SmilingWoman from "@/public/images/whyUs.jpg";
+import CardComponent from "./CardComponent";
+
+type Props = {};
+
+function WhyUsSection({}: Props) {
+  return (
+    <div className="w-full flex items-center my-14 lg:my-20 md:pb-0 lg:min-h-screen">
+    </div>
+  );
+}
+
+export default WhyUsSection;
