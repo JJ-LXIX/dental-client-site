2713cefc9b38c8a3e1906c691d5c155da7d8d955
Jagrath Shetty <jagrathshettylxix@gmail.com>
2023-07-06 00:59:53 +0530

    added why us section to page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import HeroSection from "@/components/Hero Section/HeroSection";
+import WhyUsSection from "@/components/Why Us Section/WhyUsSection";
 
 export default function Home() {
   return (
     <main className="min-h-screen">
       <HeroSection />
+      <WhyUsSection />
     </main>
   );
 }
