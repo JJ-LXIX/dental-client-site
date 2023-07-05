215685089e52b8835868202ea27913e53b3db6bd
Jagrath Shetty <jagrathshettylxix@gmail.com>
2023-07-06 01:02:43 +0530

    added work cards to work section

--- components/Work Section/WorkSection.tsx
+++ components/Work Section/WorkSection.tsx
@@ -5,6 +5,16 @@ type Props = {};
 function WorkSection({}: Props) {
   return (
     <div className="mt-5 mb-5 lg:mt-0 w-full flex justify-center">
+      <div className="w-full min-h-screen grid grid-cols-2 lg:grid-cols-3 lg:max-w-5xl gap-y-10 justify-items-center ">
+        {/* Card 1 */}
+        <WorkCard />
+        {/* Card 2 */}
+        <WorkCard />
+        <WorkCard />
+        <WorkCard />
+        <WorkCard />
+        <WorkCard />
+      </div>
     </div>
   );
 }
