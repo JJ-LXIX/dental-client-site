1606ade3b0987ae44d1b14eed748dab4abc48035
Jagrath Shetty <jagrathshettylxix@gmail.com>
2023-07-06 10:33:11 +0530

    created team section component

---
+++ components/Team Section/TeamSection.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import TeamCard from "./TeamCard";
+
+type Props = {};
+
+function TeamSection({}: Props) {
+  return (
+    <div>
+      <div className="w-full px-2 lg:max-w-4xl xl:max-w-5xl mx-auto my-10 lg:space-y-10">
+    </div>
+  );
+}
+
+export default TeamSection;
