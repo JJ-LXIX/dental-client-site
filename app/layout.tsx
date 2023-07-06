5c4e053acdcdb789761b6b7e5bfec1bc5062dad5
Jagrath Shetty <jagrathshettylxix@gmail.com>
2023-07-06 10:27:40 +0530

    added scroll smooth to html

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import Navbar from "@/components/Navbar";
+import Navbar from "@/components/Navbar/Navbar";
 import "./globals.css";
 import { Inter } from "next/font/google";
 
@@ -15,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className="scroll-smooth ">
       <body className={inter.className}>
         <Navbar />
         {children}
