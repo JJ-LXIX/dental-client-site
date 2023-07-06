f65b390217485e652f2c366a3270a543640f6346
Jagrath Shetty <jagrathshettylxix@gmail.com>
2023-07-06 14:02:47 +0530

    added components to about us page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,17 @@
+import ContactFormSection from "@/components/Contact Form Section/ContactFormSection";
+import ContactSection from "@/components/Contact Section/ContactSection";
+import TestimonialSection from "@/components/Testimonial Section/TestimonialSection";
+
 export default function Home() {
-  return <main className="min-h-screen">about</main>;
+  return (
+    <main className="min-h-screen pt-28 md:pt-36">
+      <h2 className="text-4xl font-medium md:text-5xl lg:text-5xl text-center mx-auto max-w-[80%] lg:max-w-[90%]">
+        About Us
+      </h2>
+      {/* TODO: add about us section */}
+      <TestimonialSection />
+      <ContactFormSection />
+      <ContactSection />
+    </main>
+  );
 }
