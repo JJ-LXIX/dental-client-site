4c478408cc6e5ef049202b8535eca975f4e7c7ae
Jagrath Shetty <jagrathshettylxix@gmail.com>
2023-07-06 10:29:48 +0530

    added work section component to page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import HeroSection from "@/components/Hero Section/HeroSection";
 import WhyUsSection from "@/components/Why Us Section/WhyUsSection";
+import WorkSection from "@/components/Work Section/WorkSection";
 
 export default function Home() {
   return (
     <main className="min-h-screen">
       <HeroSection />
       <WhyUsSection />
+      <WorkSection />
     </main>
   );
 }
