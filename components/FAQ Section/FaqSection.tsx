21a6404c2cb7ce7c6c44f355e0f30a89aae3a4e5
Jagrath Shetty <jagrathshettylxix@gmail.com>
2023-07-06 10:31:59 +0530

    created FaqSection component

---
+++ components/FAQ Section/FaqSection.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Accordian from "./Accordian";
+
+type Props = {};
+
+function FaqSection({}: Props) {
+  return (
+    <div className="w-full mt-32 lg:mt-10 mb-10 lg:mb-0 lg:pl-10 flex flex-col md:flex-row items-center md:justify-center lg:max-w-6xl xl:mx-auto">
+    </div>
+  );
+}
+
+export default FaqSection;
