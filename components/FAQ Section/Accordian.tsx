fea30a17d51bba42c1649e26433e65edd5a6c87b
Jagrath Shetty <jagrathshettylxix@gmail.com>
2023-07-06 10:31:06 +0530

    updated styles off accordian

--- components/FAQ Section/Accordian.tsx
+++ components/FAQ Section/Accordian.tsx
@@ -9,36 +9,36 @@ type Props = {};
 
 function Accordian({}: Props) {
   return (
-    <Accordion type="single" className="mt-5" collapsible>
+    <Accordion type="single" className="mt-5 lg:max-w-lg" collapsible>
       <AccordionItem className="" value="item-1">
-        <AccordionTrigger className="text-xl text-slate-800 font-semibold">
+        <AccordionTrigger className="text-lg text-slate-800 font-semibold">
           Is it accessible?
         </AccordionTrigger>
-        <AccordionContent className="text-lg">
+        <AccordionContent className="">
           Yes. It adheres to the WAI-ARIA design pattern.
         </AccordionContent>
       </AccordionItem>
       <AccordionItem value="item-2">
-        <AccordionTrigger className="text-xl text-slate-800 font-semibold">
+        <AccordionTrigger className="text-lg text-slate-800 font-semibold">
           Is it accessible?
         </AccordionTrigger>
-        <AccordionContent className="text-lg">
+        <AccordionContent className="">
           Yes. It adheres to the WAI-ARIA design pattern.
         </AccordionContent>
       </AccordionItem>
       <AccordionItem value="item-3">
-        <AccordionTrigger className="text-xl text-slate-800 font-semibold">
+        <AccordionTrigger className="text-lg text-slate-800 font-semibold">
           Is it accessible?
         </AccordionTrigger>
-        <AccordionContent className="text-lg">
+        <AccordionContent className="">
           Yes. It adheres to the WAI-ARIA design pattern.
         </AccordionContent>
       </AccordionItem>
       <AccordionItem value="item-4">
-        <AccordionTrigger className="text-xl text-slate-800 font-semibold">
+        <AccordionTrigger className="text-lg text-slate-800 font-semibold">
           Is it accessible?
         </AccordionTrigger>
-        <AccordionContent className="text-lg">
+        <AccordionContent className="">
           Yes. It adheres to the WAI-ARIA design pattern.
         </AccordionContent>
       </AccordionItem>
