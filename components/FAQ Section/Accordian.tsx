a0a951367be80e93a0aac3a31fb6a38ccb4a7d19
Jagrath Shetty <jagrathshettylxix@gmail.com>
2023-07-06 15:41:23 +0530

    changed accordian info

--- components/FAQ Section/Accordian.tsx
+++ components/FAQ Section/Accordian.tsx
@@ -12,34 +12,38 @@ function Accordian({}: Props) {
     <Accordion type="single" className="mt-5 lg:max-w-lg" collapsible>
       <AccordionItem className="" value="item-1">
         <AccordionTrigger className="text-lg text-slate-800 font-semibold">
-          Is it accessible?
+          Can dental procedures be performed painlessly?
         </AccordionTrigger>
         <AccordionContent className="">
-          Yes. It adheres to the WAI-ARIA design pattern.
+          Yes, modern techniques and technology ensure a comfortable experience.
         </AccordionContent>
       </AccordionItem>
       <AccordionItem value="item-2">
         <AccordionTrigger className="text-lg text-slate-800 font-semibold">
-          Is it accessible?
+          What should I do if I have a dental emergency?
         </AccordionTrigger>
         <AccordionContent className="">
-          Yes. It adheres to the WAI-ARIA design pattern.
+          Contact us immediately for guidance and, if necessary, seek prompt
+          treatment.
         </AccordionContent>
       </AccordionItem>
       <AccordionItem value="item-3">
         <AccordionTrigger className="text-lg text-slate-800 font-semibold">
-          Is it accessible?
+          How can I address dental anxiety?
         </AccordionTrigger>
         <AccordionContent className="">
-          Yes. It adheres to the WAI-ARIA design pattern.
+          Communicate your concerns with us and we can explore techniques such
+          as relaxation exercises or sedation options to help manage dental
+          anxiety.
         </AccordionContent>
       </AccordionItem>
       <AccordionItem value="item-4">
         <AccordionTrigger className="text-lg text-slate-800 font-semibold">
-          Is it accessible?
+          How often should I visit the dentist for a check-up?
         </AccordionTrigger>
         <AccordionContent className="">
-          Yes. It adheres to the WAI-ARIA design pattern.
+          Every six months, but it varies based on your oral health. Contact us
+          to get a consultancy!
         </AccordionContent>
       </AccordionItem>
     </Accordion>
