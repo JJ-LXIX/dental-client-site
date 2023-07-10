2aa5a2bd5a3fe0f3c6b95fdc8fd222fcf802eeb9
Jagrath Shetty <jagrathshettylxix@gmail.com>
2023-07-10 18:21:42 +0530

    changed styles of map component

--- components/Contact Form Section/MapComponent.jsx
+++ components/Contact Form Section/MapComponent.jsx
@@ -7,15 +7,16 @@ import "maplibre-gl/dist/maplibre-gl.css";
 
 function MapComponent({}) {
   return (
-    <div className="w-[80%] mx-auto md:w-full h-screen md:h-[50vh] lg:h-[70vh]  relative shadow-lg rounded-2xl overflow-hidden">
+    <div className="w-[80%] mx-auto md:w-full h-screen md:h-[50vh] lg:h-[70vh]  relative shadow-lg rounded-2xl overflow-hidden border-2 border-white p-2">
       <Map
         mapLib={maplibregl}
-        className="absolute inset-0"
+        className="absolute inset-0 p-2 rounded-2xl"
         initialViewState={{
           longitude: 78.041,
           latitude: 27.17115,
           zoom: 14,
         }}
+        style={{ borderRadius: "10px" }}
         mapStyle="https://api.maptiler.com/maps/streets-v2/style.json?key=vF9I9gEjq30xZHr86gew"
       >
         <NavigationControl position="top-left" />
