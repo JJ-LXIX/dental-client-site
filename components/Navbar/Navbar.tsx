43fb4316870761818a863b074da4f15a34d6a83a
Jagrath Shetty <jagrathshettylxix@gmail.com>
2023-07-10 18:24:23 +0530

    changed nav style to dark mode

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -12,7 +12,7 @@ function Navbar({}: Props) {
 
   return (
     <div className="w-full flex  items-center justify-center z-50">
-      <div className="bg-white px-6 flex justify-between items-center z-50 fixed top-5 mx-auto my-0 w-[90%]  rounded-full navbar h-[4rem] md:h-[5rem] lg:h-[5.5rem] md:w-[80%]">
+      <div className="bg-black text-white border-2 transition-all duration-300 px-6 flex justify-between items-center z-50 fixed top-5 mx-auto my-0 w-[90%]  rounded-full navbar h-[4rem] md:h-[5rem] lg:h-[5.5rem] md:w-[80%]">
         <Link
           href="/"
           onClick={() => {
