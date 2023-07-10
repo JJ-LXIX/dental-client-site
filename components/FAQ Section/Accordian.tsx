2c52b5d5527da9ef6b7c209f9ad2e1dc1528a5e5
Jagrath Shetty <jagrathshettylxix@gmail.com>
2023-07-10 18:22:16 +0530

    changed styles to dark mode

--- components/FAQ Section/Accordian.tsx
+++ components/FAQ Section/Accordian.tsx
@@ -11,37 +11,37 @@ function Accordian({}: Props) {
   return (
     <Accordion type="single" className="mt-5 lg:max-w-lg" collapsible>
       <AccordionItem className="" value="item-1">
-        <AccordionTrigger className="text-lg text-slate-800 font-semibold">
+        <AccordionTrigger className="text-lg text-zinc-300 font-semibold">
           Can dental procedures be performed painlessly?
         </AccordionTrigger>
-        <AccordionContent className="">
+        <AccordionContent className="text-zinc-300">
           Yes, modern techniques and technology ensure a comfortable experience.
         </AccordionContent>
       </AccordionItem>
       <AccordionItem value="item-2">
-        <AccordionTrigger className="text-lg text-slate-800 font-semibold">
+        <AccordionTrigger className="text-lg text-zinc-300 font-semibold">
           What should I do if I have a dental emergency?
         </AccordionTrigger>
-        <AccordionContent className="">
+        <AccordionContent className="text-zinc-300">
           Contact us immediately for guidance and, if necessary, seek prompt
           treatment.
         </AccordionContent>
       </AccordionItem>
       <AccordionItem value="item-3">
-        <AccordionTrigger className="text-lg text-slate-800 font-semibold">
+        <AccordionTrigger className="text-lg text-zinc-300 font-semibold">
           How can I address dental anxiety?
         </AccordionTrigger>
-        <AccordionContent className="">
+        <AccordionContent className="text-zinc-300">
           Communicate your concerns with us and we can explore techniques such
           as relaxation exercises or sedation options to help manage dental
           anxiety.
         </AccordionContent>
       </AccordionItem>
       <AccordionItem value="item-4">
-        <AccordionTrigger className="text-lg text-slate-800 font-semibold">
+        <AccordionTrigger className="text-lg text-zinc-300 font-semibold">
           How often should I visit the dentist for a check-up?
         </AccordionTrigger>
-        <AccordionContent className="">
+        <AccordionContent className="text-zinc-300">
           Every six months, but it varies based on your oral health. Contact us
           to get a consultancy!
         </AccordionContent>
