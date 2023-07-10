f76e9d17d0b9e3a4fad54fbaca9fb172f344b440
Jagrath Shetty <jagrathshettylxix@gmail.com>
2023-07-10 18:26:49 +0530

    changed styles of work card

--- components/Work Section/WorkCard.tsx
+++ components/Work Section/WorkCard.tsx
@@ -1,19 +1,20 @@
-import DentalWork from "@/public/images/pexels-anna-shvets-3845765.jpg";
+import DentalWork from "@/public/images/dentist-work.jpg";
 import Image from "next/image";
 type Props = {};
 
 function WorkCard({}: Props) {
   return (
-    <div className="h-[18rem] md:h-[25rem] w-[90%] relative group shadow-xl overflow-hidden rounded-2xl">
-      <div className="absolute bg-red-200 inset-0 rounded-full ">
+    <div className="h-[18rem] md:h-[25rem] w-[90%] relative group shadow-xl overflow-hidden rounded-2xl border-white border-2">
+      <div className="absolute inset-0 overflow-hidden p-2">
         <Image
           src={DentalWork}
           fill
           alt="dental work"
           style={{ objectFit: "cover" }}
+          className="p-2 rounded-2xl"
         />
       </div>
-      <div className="absolute inset-0 z-2 bg-gradient-to-b from-transparent to-black group-hover:bg-black/90 transition-all duration-300"></div>
+      <div className="absolute inset-0 z-2 bg-gradient-to-b from-transparent from-60% to-black group-hover:bg-black/90 transition-all duration-300"></div>
       <div className="cursor-default">
         <h3 className="z-10 text-white font-bold absolute bottom-5 left-3 lg:left-5 md:text-3xl group-hover:bottom-[13rem]  transition-all duration-300">
           Treatment Name
