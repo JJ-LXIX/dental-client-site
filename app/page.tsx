e157d1083407b1518672d70dc3173fa3562accbb
Jagrath Shetty <jagrathshettylxix@gmail.com>
2023-07-10 18:28:57 +0530

    added overflow hidden

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import WorkSection from "@/components/Work Section/WorkSection";
 
 export default function Home() {
   return (
-    <main className="min-h-screen">
+    <main className="min-h-screen overflow-hidden">
       <HeroSection />
       <WhyUsSection />
       <WorkSection />
