04428a5c8571b5d5fa07eadc54adb6c5ce5c4b75
Jagrath Shetty <jagrathshettylxix@gmail.com>
2023-07-10 18:38:48 +0530

    added testimonial section to page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import FaqSection from "@/components/FAQ Section/FaqSection";
 import HeroSection from "@/components/Hero Section/HeroSection";
 import TeamSection from "@/components/Team Section/TeamSection";
+import TestimonialSection from "@/components/Testimonial Section/TestimonialSection";
 import WhyUsSection from "@/components/Why Us Section/WhyUsSection";
 import WorkSection from "@/components/Work Section/WorkSection";
 
@@ -12,6 +13,7 @@ export default function Home() {
       <WorkSection />
       <FaqSection />
       <TeamSection />
+      <TestimonialSection />
     </main>
   );
 }
