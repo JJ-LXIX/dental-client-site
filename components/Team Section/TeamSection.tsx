1d3205a93f2ba35cf7f711aabb067dfa27607c91
Jagrath Shetty <jagrathshettylxix@gmail.com>
2023-07-10 18:27:42 +0530

    changed styles to dark mode

--- components/Team Section/TeamSection.tsx
+++ components/Team Section/TeamSection.tsx
@@ -5,14 +5,14 @@ type Props = {};
 
 function TeamSection({}: Props) {
   return (
-    <div>
-      <div className="w-full px-2 lg:max-w-4xl xl:max-w-5xl mx-auto my-10 lg:space-y-10">
+    <div className="bg-black">
+      <div className="w-full px-2 lg:max-w-4xl xl:max-w-5xl mx-auto py-20 lg:space-y-10">
         {/* Text */}
         <div className="space-y-2">
-          <h2 className="text-center text-4xl lg:text-5xl font-medium">
+          <h2 className="text-center text-white text-4xl lg:text-5xl font-medium">
             Our Expert Team
           </h2>
-          <h3 className="text-center text-lg text-slate-700">
+          <h3 className="text-center text-lg text-zinc-300">
             Our experienced team at Dentica is dedicated to providing
             high-quality service and personalized care to each of our clients.
           </h3>
