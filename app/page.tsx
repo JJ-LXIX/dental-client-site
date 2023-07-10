90dbb3d30cc980080ee91bdec517771ba9a96694
Jagrath Shetty <jagrathshettylxix@gmail.com>
2023-07-10 18:39:26 +0530

    added contact form section

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import FaqSection from "@/components/FAQ Section/FaqSection";
-import HeroSection from "@/components/Hero Section/HeroSection";
+import HeroSection from "@/components/Hero Section/HeroSection2";
 import TeamSection from "@/components/Team Section/TeamSection";
 import TestimonialSection from "@/components/Testimonial Section/TestimonialSection";
 import WhyUsSection from "@/components/Why Us Section/WhyUsSection";
 import WorkSection from "@/components/Work Section/WorkSection";
+import ContactFormSection from "@/components/Contact Form Section/ContactFormSection";
 
 export default function Home() {
   return (
@@ -14,6 +15,7 @@ export default function Home() {
       <FaqSection />
       <TeamSection />
       <TestimonialSection />
+      <ContactFormSection />
     </main>
   );
 }
