efe0bc92a80432d8b397ab1ce819dbcd3e946c40
Jagrath Shetty <jagrathshettylxix@gmail.com>
2023-07-13 21:56:30 +0530

    added props to work cards

--- components/Work Section/WorkSection.tsx
+++ components/Work Section/WorkSection.tsx
@@ -6,12 +6,12 @@ function WorkSection({}: Props) {
   return (
     <div className="py-10 bg-black w-full flex justify-center">
       <div className="w-full min-h-screen grid grid-cols-2 lg:grid-cols-3 lg:max-w-5xl gap-y-10 justify-items-center ">
-        <WorkCard />
-        <WorkCard />
-        <WorkCard />
-        <WorkCard />
-        <WorkCard />
-        <WorkCard />
+        <WorkCard number={1} />
+        <WorkCard number={2} />
+        <WorkCard number={3} />
+        <WorkCard number={4} />
+        <WorkCard number={5} />
+        <WorkCard number={6} />
       </div>
     </div>
   );
