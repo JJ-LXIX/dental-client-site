bcf1b11ba3b3a79dea6f4b7a000539b42355dfb0
Jagrath Shetty <jagrathshettylxix@gmail.com>
2023-07-13 21:56:50 +0530

    added framer motion animations

--- components/Work Section/WorkCard.tsx
+++ components/Work Section/WorkCard.tsx
@@ -1,10 +1,63 @@
+"use client";
 import DentalWork from "@/public/images/dentist-work.jpg";
 import Image from "next/image";
-type Props = {};
+import { motion } from "framer-motion";
+import { useEffect, useState } from "react";
 
-function WorkCard({}: Props) {
+type Props = {
+  number: number;
+};
+
+const variants = {
+  initial: {
+    opacity: 0,
+  },
+  animate: {
+    opacity: 1,
+  },
+};
+
+function WorkCard({ number }: Props) {
+  const [isMobile, setIsMobile] = useState(false);
+  useEffect(() => {
+    if (window.innerWidth < 768) {
+      setIsMobile(true);
+    }
+  }, []);
+  if (isMobile)
+    return (
+      <div className="h-[18rem] md:h-[25rem] w-[90%] relative group shadow-xl overflow-hidden rounded-2xl border-white border-2">
+        <div className="absolute inset-0 overflow-hidden p-2">
+          <Image
+            src={DentalWork}
+            fill
+            alt="dental work"
+            style={{ objectFit: "cover" }}
+            className="p-2 rounded-2xl"
+            placeholder="blur"
+          />
+        </div>
+        <div className="absolute inset-0 z-2 bg-gradient-to-b from-transparent from-60% to-black group-hover:bg-black/90 transition-all duration-300"></div>
+        <div className="cursor-default">
+          <h3 className="z-10 text-white font-bold absolute bottom-5 left-3 lg:left-5 md:text-3xl group-hover:bottom-[13rem]  transition-all duration-300">
+            Treatment Name
+          </h3>
+          <h3 className="opacity-0 group-hover:opacity-100 transition-all duration-300 z-10 md:text-xl text-white/70  absolute -bottom-10 group-hover:bottom-5 left-3 lg:left-5">
+            Treatment details Lorem, ipsum dolor sit amet consectetur
+            adipisicing elit Lorem ipsum dolor sit amet consectetur.
+          </h3>
+        </div>
+      </div>
+    );
   return (
-    <div className="h-[18rem] md:h-[25rem] w-[90%] relative group shadow-xl overflow-hidden rounded-2xl border-white border-2">
+    <motion.div
+      variants={variants}
+      initial="initial"
+      whileInView="animate"
+      transition={{ delay: 0.1 * number }}
+      viewport={{ once: true }}
+      className="h-[18rem] md:h-[25rem] w-[90%] relative group shadow-xl overflow-hidden rounded-2xl border-white border-2"
+    >
       <div className="absolute inset-0 overflow-hidden p-2">
         <Image
           src={DentalWork}
@@ -25,7 +78,7 @@ function WorkCard({}: Props) {
           elit Lorem ipsum dolor sit amet consectetur.
         </h3>
       </div>
-    </div>
+    </motion.div>
   );
 }
 
